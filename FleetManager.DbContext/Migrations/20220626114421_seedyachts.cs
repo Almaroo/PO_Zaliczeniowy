bec45a6b6b6bbb87b77fb902904d5958fd337331
Almaroo <mfxbarszcz@gmail.com>
2022-06-26 14:02:45 +0200

    Seed yachts data

---
+++ FleetManager.DbContext/Migrations/20220626114421_seedyachts.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FleetManager.YachtsContext.Migrations
+{
+    public partial class seedyachts : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                "Yachts",
+                new[] { "YachtId", "Name", "YearBuilt", "Make" },
+                new object[,]
+                {
+                    { 1, "Antibes", 2008, "Oceanis" },
+                    { 2, "Big Blue", 2005, "Sun Odyssey" },
+                    { 3, "Alegro", 2018, "Dufour" },
+                    { 4, "Bella", 2022, "Bavaria" },
+                }
+                );
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                "Yachts",
+                "YachtId",
+                1
+                );
+            migrationBuilder.DeleteData(
+                "Yachts",
+                "YachtId",
+                2
+                );
+            migrationBuilder.DeleteData(
+                "Yachts",
+                "YachtId",
+                3
+                );
+            migrationBuilder.DeleteData(
+                "Yachts",
+                "YachtId",
+                4
+                );
+            migrationBuilder.DeleteData(
+                "Yachts",
+                "YachtId",
+                5
+                );
+        }
+    }
+}
