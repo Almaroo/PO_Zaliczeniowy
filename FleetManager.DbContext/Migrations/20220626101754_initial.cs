38832cf2759eb9210d9f0a1217f5ea8dbebc6e66
Almaroo <mfxbarszcz@gmail.com>
2022-06-26 12:19:45 +0200

    Complete DbContext, create first migration

---
+++ FleetManager.DbContext/Migrations/20220626101754_initial.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FleetManager.YachtsContext.Migrations
+{
+    public partial class initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
--- FleetManager.DbContext/YachtsContext.cs
+++ FleetManager.DbContext/YachtsContext.cs
@@ -4,6 +4,10 @@ namespace FleetManager.YachtsContext
 {
     public class YachtsContext : DbContext
     {
-
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=.;Initial Catalog=FleetManagerDev;Trusted_Connection=True;MultipleActiveResultSets=true;");
+            optionsBuilder.LogTo(Console.WriteLine);
+        }
     }
 }
